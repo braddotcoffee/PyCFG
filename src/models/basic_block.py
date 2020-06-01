cb207c48abaef7d36855d6cd5b8cbdb0ec1048de
Bradford Bonanno <bwbonanno@wpi.edu>
2020-06-01 15:21:33 -0400

    Updated Basic Block

--- src/models/basic_block.py
+++ src/models/basic_block.py
@@ -1,10 +1,40 @@
-from typing import List
-from ast import AST, stmt
+from typing import List, Union
+from ast import AST, expr, stmt, Call, Expr
+from lazy import lazy
 
 
 class BasicBlock:
+    """
+    A BasicBlock encapsulates a straight line code sequence
+
+    :param body (List[AST]): AST nodes which make up the BasicBlock
+
+    :param inbound (List[BasicBlock]): BasicBlocks which call this one
+
+    :param outbound (List[BasicBlock]): BasicBlocks which this one may call
+    """
+
+    def __init__(
+        self,
+        body: List[AST] = None,
+        inbound: List["BasicBlock"] = None,
+        outbound: List["BasicBlock"] = None,
+    ):
+        self.body = body if body is not None else list()
+        self.inbound = inbound if inbound is not None else list()
+        self.outbound = outbound if outbound is not None else list()
+
+    @lazy
+    def function_calls(self):
+        """
+        All of the function calls which are a part of this BasicBlock
+        """
+        return list(
+            filter(lambda ast_node: isinstance(ast_node, Call), self.body)
+        )
+
     @staticmethod
-    def _validate_ast_nodes(ast: List[AST]):
+    def _validate_ast_node(ast: List[AST]) -> List[Union[expr, stmt]]:
         """
         Ensure that ast_node is a statement (stmt)
 
@@ -13,11 +43,24 @@ class BasicBlock:
         :raise: ValueError if ast_node is not a subclass of stmt
         """
         for ast_node in ast:
-            if not isinstance(ast_node, stmt):
+            if not isinstance(ast_node, expr) and not isinstance(
+                ast_node, stmt
+            ):
                 raise ValueError("Invalid AST node provided")
+        return ast
 
     @staticmethod
-    def build_from_ast(ast: List[stmt]) -> "BasicBlock":
+    def _build_body(ast: List[Union[expr, stmt]]):
+        body = []
+        for ast_node in ast:
+            if isinstance(ast_node, Expr):
+                body.append(ast_node.value)
+            else:
+                body.append(ast_node)
+        return body
+
+    @staticmethod
+    def build_from_ast(ast: List[AST]) -> "BasicBlock":
         """
         Builds a single basic block from its AST
 
@@ -27,6 +70,7 @@ class BasicBlock:
 
         :raise: ValueError if ast not a list of stmt
         """
-        BasicBlock._validate_ast_nodes(ast)
+        ast = BasicBlock._validate_ast_node(ast)
+        body = BasicBlock._build_body(ast)
 
-        return BasicBlock()
+        return BasicBlock(body=body)
--- test/Models/test_basic_block.py
+++ test/Models/test_basic_block.py
@@ -1,19 +1,52 @@
 import unittest
 from src.models.basic_block import BasicBlock
-from ast import Expr, BoolOp
+from ast import Expr, parse, Call, Constant
 
 
 class TestBasicBlockFromAst(unittest.TestCase):
     def test_build_returns_BasicBlock(self):
-        sample_ast = [Expr()]
+        sample_ast = [Expr(Constant())]
         basic_block = BasicBlock.build_from_ast(sample_ast)
         self.assertIsInstance(basic_block, BasicBlock)
 
-    def test_build_errors_with_incorrect_node(self):
+    def test_build_errors_with_non_ast(self):
         sample_ast = [None]
         with self.assertRaises(ValueError):
             BasicBlock.build_from_ast(sample_ast)
 
-        sample_ast = [BoolOp()]
+        sample_ast = [[]]
         with self.assertRaises(ValueError):
             BasicBlock.build_from_ast(sample_ast)
+
+    def test_build_single_expression(self):
+        sample_code = "print('Hello, world!')"
+        module = parse(sample_code)
+        function_call = module.body[0].value
+        basic_block = BasicBlock.build_from_ast(module.body)
+        self.assertIsInstance(basic_block, BasicBlock)
+        self.assertEqual(len(basic_block.body), 1)
+        self.assertEqual(basic_block.body[0], function_call)
+        self.assertIsInstance(basic_block.body[0], Call)
+
+    def test_build_multiple_expressions(self):
+        sample_code = "print('Hello Number 1')\n"
+        sample_code += "print('Hello World 2')\n"
+        sample_code += "print('Hello World 3')"
+        module = parse(sample_code)
+        basic_block = BasicBlock.build_from_ast(module.body)
+        self.assertIsInstance(basic_block, BasicBlock)
+        self.assertEqual(len(basic_block.body), 3)
+
+
+class TestBasicBlockFunctionCalls(unittest.TestCase):
+    def test_function_calls_returns_only_calls(self):
+        sample_code = "print('Hello Number 1')\n"
+        sample_code += "print('Hello World 2')\n"
+        sample_code += "def testFunc():\n"
+        sample_code += "    pass"
+
+        module = parse(sample_code)
+        basic_block = BasicBlock.build_from_ast(module.body)
+        self.assertEqual(len(basic_block.function_calls), 2)
+        self.assertIsInstance(basic_block.function_calls[0], Call)
+        self.assertIsInstance(basic_block.function_calls[1], Call)
